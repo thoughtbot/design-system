81b4c2433b25bb155f5cc83c3d876a70297acac0
dimanyc <dimitrynazarov@gmail.com>
2019-10-27 20:53:45 -0400

    updates ViewHelper to allow passing `**args`
    as opposed to passing keyword args of `shape:` and `size:`
    
    re: https://github.com/thoughtbot/design-system/pull/134#issuecomment-545588907

--- spec/tbds/view_helpers_spec.rb
+++ spec/tbds/view_helpers_spec.rb
@@ -7,21 +7,33 @@ RSpec.describe Tbds::ViewHelpers do
 
       allow(AvatarPresenter).to receive(:new).and_return(presenter_double)
 
-      TestCase.new.tbds_avatar(**options)
+      TestCase.new.tbds_avatar(**avatar_options)
 
       expect(presenter_double).to have_received(:render)
     end
 
-    private
+    context "when shape and size arguments are missing" do
+      it "does not raise errors" do
+        presenter_double = double(AvatarPresenter, render: true)
 
-    def options
-      {
-        alt_text: "foo",
-        path: "/foo.jpg",
-        shape: "cirlce",
-        size: "small",
-      }
+        allow(AvatarPresenter).to receive(:new).and_return(presenter_double)
+
+        expect do
+          TestCase.new.tbds_avatar(**avatar_options.except(:shape, :size))
+        end.not_to raise_error
+      end
     end
+  end
+
+  private
+
+  def avatar_options
+    {
+      alt_text: "foo",
+      path: "/foo.jpg",
+      shape: "cirlce",
+      size: "small",
+    }
   end
 end
 
