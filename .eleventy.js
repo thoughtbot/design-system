93593387678be9849d79fd1012c8f3a335519881
LkeMitchll <mail@luke.fail>
2021-09-17 14:44:04 +0100

    Replace scss shortcode with npm scripts

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,8 @@
-const sass = require("sass");
-const fs = require("fs").promises;
-
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget("./**/*.scss");
-  eleventyConfig.addShortcode("stylesheet", (input) => {
-    const outputFilename = input.split("/")[1].split(".")[0];
-    sass.render(
-      {
-        file: input,
-      },
-      (error, result) => {
-        fs.writeFile(`_site/${outputFilename}.css`, result.css, function (err) {
-          if (err) {
-            throw new Error(error);
-          }
-        });
-      }
-    );
-    return `<link rel='stylesheet' type='text/css' href='/${outputFilename}.css' />`;
+  // Reload borwser when CSS changes
+  eleventyConfig.addWatchTarget("_site/*.css");
+  eleventyConfig.setBrowserSyncConfig({
+    files: ["_site/*.css"],
   });
 
   return {
