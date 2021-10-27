bb9c58a350cc606bcfb670fb611ea705229c5a9a
Stephen Lindberg <stwelin@gmail.com>
2021-10-27 10:18:34 -0400

    Add basic styling to docs site
    * Basic page layout and link styling
    * Syntax highlighting (plugin)
    * Inter font

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,15 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function (eleventyConfig) {
   // Reload browser when CSS changes
   eleventyConfig.addWatchTarget("_site/*.css");
+  eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.setBrowserSyncConfig({
     files: ["_site/*.css"],
   });
 
   return {
+    htmlTemplateEngine: "njk",
     dir: {
       includes: "docs/_includes",
     },
